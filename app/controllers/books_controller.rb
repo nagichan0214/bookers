1473cc583bfc1114886d621ebc5ce0ceafcd0146
nagichan0214 <ec2-user@ip-172-31-15-20.ap-northeast-1.compute.internal>
2024-01-11 12:57:34 +0000

    [finish}destroy

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,6 +11,7 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
@@ -18,8 +19,22 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
-
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+  end
+    
+ 
   private
   def book_params
     params.require(:book).permit(:title, :body)
