959092e2ae3a8dc5c3b2d2b81a93bc1cd09f4885
nagichan0214 <ec2-user@ip-172-31-15-20.ap-northeast-1.compute.internal>
2024-01-13 09:47:07 +0000

    [finish]all

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,14 +4,15 @@ class BooksController < ApplicationController
   end
 
  def create
-   book = Book.new(book_params)
-   if book.save
+   @book = Book.new(book_params)
+   if @book.save
      flash[:notice] = "Book was successfully created."
-   redirect_to book_path(book.id)
- else
-   render :new
+   redirect_to book_path(@book.id)
+   else
+    @books = Book.all
+   render :index
+   end
  end
-end
 
   def index
     @books = Book.all
@@ -24,15 +25,20 @@ end
 
   def edit
     @book = Book.find(params[:id])
+    if @book.save
+    else
+      render :edit
+    end
   end
 
   def update
-    book = Book.find(params[:id])
+    @book = Book.find(params[:id])
+    book = @book
     if book.update(book_params)
       flash[:notice] = "Book was successfully updated."
     redirect_to book_path(book.id)
     else
-  render :new
+      render :edit
     end
   end
 
